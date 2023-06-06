87888aa4520f68c3e42c7499c3982ef3923c997f
Peteranny <peteranny2@gmail.com>
2023-06-07 02:47:26 +0800

    apply font

--- PicCollageTakeHomeQuiz/AppDelegate.swift
+++ PicCollageTakeHomeQuiz/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import RxSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,8 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let window = UIWindow()
 
         let manager = FontManager()
-        let fontSelectorViewModel = FontSelectorViewModel(manager: manager)
-        window.rootViewController = ViewController(fontSelectorViewModel: fontSelectorViewModel)
+        window.rootViewController = ViewController(manager: manager)
         window.makeKeyAndVisible()
 
         self.window = window
