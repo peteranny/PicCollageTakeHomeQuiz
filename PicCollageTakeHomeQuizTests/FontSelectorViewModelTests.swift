b9cc90541560398cb581173bcef383ad85cbbbfd
Peteranny <peteranny2@gmail.com>
2023-06-07 14:09:14 +0800

    unit test models

--- PicCollageTakeHomeQuizTests/FontSelectorViewModelTests.swift
+++ PicCollageTakeHomeQuizTests/FontSelectorViewModelTests.swift
@@ -86,4 +86,44 @@ class FontSelectorViewModelTests: XCTestCase {
             (200, .input(.all)),
         ])
     }
+
+    /// Test if `outputs.models` emits the models that match the selected category
+    func test_models() {
+        // Set up dependencies
+        let items: [FontItem] = [
+            FontItem.mock(family: "1", category: "a"),
+            FontItem.mock(family: "2", category: "b"),
+        ]
+        let manager = FontManager.mock(fetchedItems: items)
+        let viewModel = FontSelectorViewModel.mock(manager: manager)
+
+        // Set up inputs / outputs
+        let selectedCategory = PassthroughSubject<FontCategory, Never>()
+        let inputs = FontSelectorViewModel.Inputs.mock(
+            selectedCategory: selectedCategory.eraseToAnyPublisher()
+        )
+        let outputs = viewModel.bind(inputs)
+
+        // Bind observers
+        let familiesObserver = ReplaySubject<[String], Never>.createUnbounded()
+        var cancellables: [AnyCancellable] = []
+        cancellables.append(contentsOf: outputs.bindings)
+        cancellables.append(outputs.models.map({ $0.map(\.item.family) }).sink { familiesObserver.send($0) })
+
+        // Start steps
+        let scheduler = TestScheduler(initialClock: .zero)
+        let results = scheduler.start {
+            selectedCategory.send(.specific("a"))
+            selectedCategory.send(.all)
+            return familiesObserver
+        }
+
+        // Verify the result
+        XCTAssertEqual(results.recordedOutput, [
+            (200, .subscription),
+            (200, .input(["1", "2"])),
+            (200, .input(["1"])),
+            (200, .input(["1", "2"])),
+        ])
+    }
 }
