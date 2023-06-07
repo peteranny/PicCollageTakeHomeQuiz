f64d04f3ad3096897584f37989fcfc683868641c
Peteranny <peteranny2@gmail.com>
2023-06-07 14:03:57 +0800

    unit test selected category

--- PicCollageTakeHomeQuizTests/FontSelectorViewModelTests.swift
+++ PicCollageTakeHomeQuizTests/FontSelectorViewModelTests.swift
@@ -39,4 +39,43 @@ class FontSelectorViewModelTests: XCTestCase {
             [.all, .specific("a"), .specific("b")],
         ])
     }
+
+    /// Test if `outputs.selectedCategory` responds to the inputs
+    func test_selectedCategory() {
+        // Set up dependencies
+        let items: [FontItem] = [
+            FontItem.mock(category: "a"),
+            FontItem.mock(category: "b"),
+        ]
+        let manager = FontManager.mock(fetchedItems: items)
+        let viewModel = FontSelectorViewModel.mock(manager: manager)
+
+        // Set up inputs / outputs
+        let selectedCategory = PublishSubject<FontCategory>()
+        let inputs = FontSelectorViewModel.Inputs.mock(
+            selectedCategory: selectedCategory.asObservable()
+        )
+        let outputs = viewModel.bind(inputs)
+
+        // Bind observers
+        let selectedCategoryObserver = ReplaySubject<FontCategory>.createUnbounded()
+        let disposeBag = DisposeBag()
+        disposeBag.insert(outputs.bindings)
+        disposeBag.insert(outputs.selectedCategory.drive(selectedCategoryObserver))
+
+        // Steps
+        selectedCategory.onNext(.specific("b"))
+        selectedCategory.onNext(.specific("a"))
+        selectedCategory.onNext(.all)
+
+        // Verify the result
+        selectedCategoryObserver.onCompleted()
+        let selectedCategoryOut = try! selectedCategoryObserver.toBlocking().toArray()
+        XCTAssertEqual(selectedCategoryOut, [
+            .all,
+            .specific("b"),
+            .specific("a"),
+            .all,
+        ])
+    }
 }
